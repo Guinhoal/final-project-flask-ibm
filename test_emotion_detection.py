f2c5fd7742ac0522ff9345d303c66bb683b32a94
Thiago Arthur <thiagodevprof@gmail.com>
2025-02-12 15:35:07 -0300

    New features in unit tests

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,8 +1,11 @@
 from EmotionDetection import sentiment_analyzer
+import json
 
 print("Unit Test for Emotion Detection")
-sentiment_analyzer("I am glad this happened") # Expected output: "joy"
-sentiment_analyzer("I am really mad about this") # Expected output: "anger"
-sentiment_analyzer("I feel disgusted just hearing about this") # Expected output: "disgust"
-sentiment_analyzer("I am so sad about this") # Expected output: "sadness"
-sentiment_analyzer("I am really afraid that this will happen") # Expected output: "fear"
+joy = json.loads(sentiment_analyzer("I am glad this happened")) # Expected output: "joy"
+anger = json.loads(sentiment_analyzer("I am really mad about this")) # Expected output: "anger"
+disgust = json.loads(sentiment_analyzer("I feel disgusted just hearing about this")) # Expected output: "disgust"
+sadness = json.loads(sentiment_analyzer("I am so sad about this")) # Expected output: "sadness"
+fear = json.loads(sentiment_analyzer("I am really afraid that this will happen")) # Expected output: "fear"
+
+print("Dominante ", joy[dominant])
